c683e99402d139fed06e65cc7183c9cc26eefd0c
alaa129 <alaa1292002.a@gmail.com>
2023-09-08 04:27:56 +0300

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * Description: Prints alphabet in one line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("hello,world");
+        return (0);
+
+}
