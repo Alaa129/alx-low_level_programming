30d4d931a9e6ed498ab86a9cec0c4d298a09eb6d
alaa129 <alaa1292002.a@gmail.com>
2023-09-09 03:38:09 +0300

    8-print_base16.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,12 @@
 int main(void)
 {
 	int digit = 0;
+	
 	while (digit <= 9)
 	{
 		/*convert digit to ASCII representation*/
 		putchar(digit + 48);
-		if (digit != '9')
+		if (digit != 9)
 		{
 			putchar(',');
 			putchar(' ');
