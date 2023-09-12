90ba230bb4a9dba8b7d10ab77de6101f675a8fc8
alaa129 <alaa1292002.a@gmail.com>
2023-09-13 00:48:28 +0300

    enshaldiging

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_to_98 - print n to 98 counts
+* separated by comma, followed by space
+*
+* @n: input
+*/
+
+void print_to_98(int n)
+{
+	int count;
+
+	while (n != 98)
+	{
+	printf("%d ", n);
+	if (n < 98)
+	n++;
+	else
+	n--;
+	}
+	printf("%d\n", n);
+}
