64943578ad38f0465a1055cc79f4530d030879ac
alaa129 <alaa1292002.a@gmail.com>
2023-09-15 00:23:02 +0300

    nein

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,11 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 	if (num % 3 == 0 && !(num % 5 == 0))
-	printf("fizz");
+	printf("Fizz");
 	else if (num % 5 == 0 && !(num % 3 == 0))
 	printf("Buzz");
 	else if (num % 3 == 0 && num % 5 == 0)
-	printf("fizzBuzz");
+	printf("FizzBuzz");
 	else
 	printf("%d", num);
 
