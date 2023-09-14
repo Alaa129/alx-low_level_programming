68a38e11fff3bc06d91c90e1293a5b152ab2c6c4
alaa129 <alaa1292002.a@gmail.com>
2023-09-14 21:40:29 +0300

    hello

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of a specified size using #
- * @size: the size of the square
+ * print_diagonal - draws a diagonal line of '\' characters
+ * @n: the number of times '\' should be printed
  */
- 
-void print_square(int size)
-{
-	int row, col;
 
-	if (size <= 0)
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
 	{
 	_putchar('\n');
 	return;
 	}
-	for (row = 0; row < size; row++)
+	for (i = 0; i < n; i++)
 	{
-	for (col = 0; col < size; col++)
+	for (j = 0; j < i; j++)
 	{
-	_putchar('#');
+	_putchar(' ');
 	}
+	_putchar('\\');
 	_putchar('\n');
 	}
 }
