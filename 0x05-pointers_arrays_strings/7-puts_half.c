fd10c9f3a537d2af3fb8d865329170a1390ccd4b
alaa129 <alaa1292002.a@gmail.com>
2023-09-18 13:46:26 +0300

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* puts2 - function should print only one character out of two
+* starting with the first one
+* @str:  Input
+* Return: print
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	
+}
