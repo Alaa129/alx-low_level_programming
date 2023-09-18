01f8373ef54021045a8739400e7a61bbfe646e6d
alaa129 <alaa1292002.a@gmail.com>
2023-09-18 14:14:51 +0300

    -puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,15 +1,24 @@
 #include "main.h"
 
 /**
-* puts2 - function should print only one character out of two
-* starting with the first one
-* @str:  Input
-* Return: print
+* puts_half - prints the remianing of string
+* @str:  the string Input
+* Return: void
 */
 
 void puts_half(char *str)
 {
-	int i;
+	int len = 0;
+	int half, i;
 
-	
+	while (str[len] != '\0')
+	{
+	len++;
+	}
+	half = (len - 1) / 2;
+	for (i = (half + 1); i < len; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
 }
