0a62a4a57b2c34a2cfd5bc25b6d1d6d1dbad2643
Alaa129 <alaa1292002.a@gmail.com>
2023-10-31 01:59:56 +0200

    2 task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,6 +8,7 @@
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -36,4 +37,4 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(fd);
 
 	return (1);
-    
+}
